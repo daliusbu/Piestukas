a9a459101531da6b9764bf78529f20d1e0ba2519
Dalius Burokas <daliusbu@gmail.com>
2018-02-22 17:38:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+p2
+==
+
+A Symfony project created on February 22, 2018, 4:12 pm.
---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dalius
+ * Date: 18.2.22
+ * Time: 16.28
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;;
+
+
+class TestController extends Controller
+{
+
+    /**
+     * @return mixed
+     * @Route("/test", name="test")
+     */
+ public function testAction(){
+     return $this->render('Test/test.html.twig', [
+        'something'=> 'Something',
+     ]);
+ }
+}
