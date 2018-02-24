ea4a208bbd4964e80ddcf4d5deca2fdbc4a0daf9
Dalius Burokas <daliusbu@gmail.com>
2018-02-24 18:33:57 +0200

    again

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -17,23 +17,23 @@ use Symfony\Component\HttpFoundation\Request;;
 class TestController extends Controller
 {
 
-    /**
-     * @return mixed
-     * @Route("/test", name="test")
-     */
- public function testAction(){
-     $em=$this->getDoctrine()->getManager();
-     $product = new Product();
-     $product->setSkaicius(20);
-     $product->setText('dvidesimt');
-     $em->persist($product);
-     $em->flush();
-
-     $id = $product->getId();
-     return $this->render('Test/test.html.twig', [
-        'something'=> 'Product id is '.$id ,
-     ]);
- }
+//    /**
+//     * @return mixed
+//     * @Route("/test", name="test")
+//     */
+// public function testAction(){
+//     $em=$this->getDoctrine()->getManager();
+//     $product = new Product();
+//     $product->setSkaicius(20);
+//     $product->setText('dvidesimt');
+//     $em->persist($product);
+//     $em->flush();
+//
+//     $id = $product->getId();
+//     return $this->render('Test/test.html.twig', [
+//        'something'=> 'Product id is '.$id ,
+//     ]);
+// }
 
     /**
      * @return mixed
@@ -47,14 +47,14 @@ class TestController extends Controller
         $em->persist($product);
         $em->flush();
 
-       $product1 = $this->getDoctrine()
-           ->getRepository(Product::class)
-           ->find(1);
-       if (!$product1) {
-           throw $this->createNotFoundException(
-               'No product found for id 1'
-           );
-       }
+//       $product1 = $this->getDoctrine()
+//           ->getRepository(Product::class)
+//           ->find(1);
+//       if (!$product1) {
+//           throw $this->createNotFoundException(
+//               'No product found for id 1'
+//           );
+//       }
 
 
 
